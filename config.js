0faada771d02a92841df619ad3b2441641f65b22
Mandy Yip <yipmanyee@hotmail.com>
2021-06-01 17:38:13 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '210897547523700',
+        clientSecret: 'ec65b8891ebaff7834178486c1cbe76c'
+    }
 }
